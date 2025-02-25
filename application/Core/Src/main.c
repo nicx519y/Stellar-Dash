95542daa4f57c0e48edaecec91c572536864cb1d
nicx519y <nicx519y@gmail.com>
2025-02-26 00:56:55 +0800

    Reset_Handler 留在FLASH，其他代码copy到RAM执行成功

--- application/Core/Src/main.c
+++ application/Core/Src/main.c
@@ -51,7 +51,6 @@ HCD_HandleTypeDef hhcd_USB_OTG_HS;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 void PeriphCommonClock_Config(void);
-static void MPU_Config(void);
 void UserLEDClose(void);
 
 /* USER CODE BEGIN PFP */
@@ -202,31 +201,6 @@ void PeriphCommonClock_Config(void)
 
 /* USER CODE END 4 */
 
- /* MPU Configuration */
-
-void MPU_Config(void)
-{
-    MPU_Region_InitTypeDef MPU_InitStruct = {0};
-
-    HAL_MPU_Disable();
-
-    /* 配置 RAM 区域 */
-    MPU_InitStruct.Enable = MPU_REGION_ENABLE;
-    MPU_InitStruct.Number = MPU_REGION_NUMBER0;
-    MPU_InitStruct.BaseAddress = 0x24000000;
-    MPU_InitStruct.Size = MPU_REGION_SIZE_512KB;
-    MPU_InitStruct.SubRegionDisable = 0x00;
-    MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
-    MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
-    MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;  // 必须显式允许执行
-    MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
-    MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
-    MPU_InitStruct.IsBufferable = MPU_ACCESS_BUFFERABLE;
-    HAL_MPU_ConfigRegion(&MPU_InitStruct);
-
-    HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
-}
-
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
@@ -234,6 +208,7 @@ void MPU_Config(void)
 void Error_Handler(void)
 {
     /* USER CODE BEGIN Error_Handler_Debug */
+    APP_ERR("Error_Handler...\r\n");
     /* User can add his own implementation to report the HAL error return state */
     __disable_irq();
     while (1)
